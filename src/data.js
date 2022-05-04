70c5e7f9c8b281670413180be40ef3ab093ad6f0
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-05-03 23:49:38 -0400

    added cards and dummy links

---
+++ src/data.js
@@ -0,0 +1,10 @@
+
+
+const mainCardsInfo = [
+    {title: 'Work Experience', info: 'Here is my experience', link: '#'}, 
+    {title: 'Education', info: 'Here is my Education', link: '#'}, 
+    {title: 'Projects', info: 'Here is my Projects', link: '#'}, 
+    {title: 'Resume', info: 'Here is my Resume', link: '#'}
+];
+
+export default mainCardsInfo;
