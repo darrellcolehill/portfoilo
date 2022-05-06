eacafdffe989c8fac6f4af3b204a1ca5f42e3df5
Darrell Cole Hill <darrellcolehill@gmail.com>
2022-05-06 12:22:42 -0400

    updated card links

--- src/components/TopicCardDeck/data.js
+++ src/components/TopicCardDeck/data.js
@@ -2,10 +2,10 @@ const currentLocation = window.location.href;
 
 console.log(currentLocation);
 const data = [
-    {title: 'Work Experience', info: 'Here is my experience', link: `${currentLocation}/Work`}, 
-    {title: 'Education', info: 'Here is my Education', link: `${currentLocation}/Education`}, 
-    {title: 'Projects', info: 'Here is my Projects', link: `${currentLocation}/Projects`}, 
-    {title: 'Resume', info: 'Here is my Resume', link: `${currentLocation}/Resume`}
+    {title: 'Work Experience', info: 'Here is my experience', link: `${currentLocation}Work`}, 
+    {title: 'Education', info: 'Here is my Education', link: `${currentLocation}Education`}, 
+    {title: 'Projects', info: 'Here is my Projects', link: `${currentLocation}Projects`}, 
+    {title: 'Resume', info: 'Here is my Resume', link: `${currentLocation}Resume`}
 ];
 
 export default data;
